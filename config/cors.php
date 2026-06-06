ffdb9f01b56ecad5324d723596af3ffd3271e827
Arden28 <laudbouetoumoussa@gmail.com>
2026-06-06 12:26:00 +0300

    fix: updated core.php

--- config/cors.php
+++ config/cors.php
@@ -24,24 +24,16 @@ return [
         'https://console.navigo.co.ke',
         // Local dev
         'http://localhost:5173',
-        'http://localhost:4173',
-        'http://localhost:3000',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:4173',
-        'http://127.0.0.1:3000',
     ],
 
-    // Catch any other localhost/127.0.0.1 port (e.g. custom Vite port)
-    'allowed_origins_patterns' => [
-        '#^https?://(localhost|127\.0\.0\.1)(:\d+)?$#',
-    ],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 86400,
+    'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
